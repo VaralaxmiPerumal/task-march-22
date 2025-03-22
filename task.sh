102665fbc0849fa0ec3c2b10f3c9080a3e8cf230
VaralaxmiPerumal <varalaxmiperumal@gmail.com>
2025-03-22 16:07:47 +0530

    task.sh is committed from master

---
+++ task.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+main_dir=0
+while [ $main_dir -eq 0 ]
+do
+        read -p "Enter the main directory name " main
+        if [ -z $main ]
+        then
+                echo "Given directory name is null, Enter valid directory name"
+                exit 0
+        fi
+        if [ -d $main ]
+        then
+                echo " Directory name alreay exist, Enter different name"
+        else
+                mkdir $main
+                main_dir=1
+        fi
+done
+
+
+sub_dir=0
+while [ $sub_dir -eq 0 ]
+do
+        read -p "Enter the $i sub directory name" sub
+        if [ -z $main/$sub ]
+        then
+                echo "Given sub directory name is null, Enter the valid name"
+                exit 0
+        fi
+        if [ -d $main/$sub ]
+        then
+                echo " Directory name already exist, Enter different name "
+        else
+                mkdir $main/$sub
+	
+                filename=0
+		while [ $filename -eq 0 ]
+		do
+                        read -p "Enter the $j filename" file
+                        if [ -z $main/$sub/$file ]
+                        then
+                                echo "Given filename is null, Enter the valid name"
+                                exit 0
+                        fi
+                        if [ -f $main/$sub/$file ]
+                        then
+                                echo "Entered filename already exist"
+                        else
+                                touch $main/$sub/$file
+                                filename=1
+                        fi
+                done
+
+        sub_dir=1
+	fi
+done
+
